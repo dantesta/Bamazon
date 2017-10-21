e5be02bf5be2a47d8e948cb55b432440eb99efc3
Dan Testa <dantesta@Dans-MacBook-Pro-3.local>
2017-10-20 22:21:27 -0400

    products print

--- customer.js
+++ customer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var table = require("console.table");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -14,5 +15,19 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
  
-
 });
+
+function queryProducts() {
+    connection.query("SELECT id, product_name, price FROM products", function(error, results) {
+        if (error) throw error;
+
+        console.log("\nWelcome to Bamazon: PRODUCTS FOR SALE");
+        console.log("===========");
+
+        console.table(results);
+        console.log("===========");
+        makePurchase();
+    });
+}
+
+queryProducts();
